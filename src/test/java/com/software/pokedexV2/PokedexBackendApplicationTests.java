22866a89094ae2cb4f1cce785ac5eae39015f850
Calderon0723 <00215818@uca.edu.sv>
2025-11-12 18:05:02 -0600

    Initial commit - Pokedex Backend Spring Boot :tada:

---
+++ src/test/java/com/software/pokedexV2/PokedexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.pokedexV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
