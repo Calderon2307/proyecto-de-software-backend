43f362eb9a22b0dfd6f72d1a3c8e7b95126bc0a7
Calderon0723 <00215818@uca.edu.sv>
2025-11-13 15:30:52 -0600

    ADD: Entrenador Mapper :sparkles:

---
+++ src/main/java/com/software/pokedexV2/mapper/EntrenadorMapper.java
@@ -0,0 +1,69 @@
+package com.software.pokedexV2.mapper;
+
+import com.software.pokedexV2.dto.request.Entrenador.EntrenadorRequest;
+import com.software.pokedexV2.dto.request.Entrenador.EntrenadorUpdateRequest;
+import com.software.pokedexV2.dto.response.Entrenador.EntrenadorResponse;
+import com.software.pokedexV2.dto.response.Pokemon.PokemonResponse;
+import com.software.pokedexV2.entities.Entrenador;
+import com.software.pokedexV2.entities.Pokemon;
+
+import java.util.List;
+
+public class EntrenadorMapper {
+
+    private EntrenadorMapper() {}
+
+    public static Entrenador toEntityCreate(
+            EntrenadorRequest nuevoEntrenador,
+            Pokemon pokemonFavorito
+    ) {
+       return Entrenador.builder()
+               .nombre(nuevoEntrenador.getNombre())
+               .email(nuevoEntrenador.getEmail())
+               .contrasena(nuevoEntrenador.getContrasena())
+               .regionPreferida(nuevoEntrenador.getRegionPreferida())
+               .tipoPreferido(nuevoEntrenador.getTipoPreferido())
+               .pokemonPreferido(pokemonFavorito)
+               .build();
+    }
+
+    public static void toEntityUpdate(
+            Entrenador entrenador,
+            EntrenadorUpdateRequest updateReq,
+            Pokemon pokemonFavorito
+    ){
+        if(pokemonFavorito != null) entrenador.setPokemonPreferido(pokemonFavorito);
+        if(updateReq.getNombre() != null) entrenador.setNombre(updateReq.getNombre());
+        if(updateReq.getRegionPreferida() != null) entrenador.setRegionPreferida(updateReq.getRegionPreferida());
+        if(updateReq.getTipoPreferido() != null) entrenador.setTipoPreferido(updateReq.getTipoPreferido());
+    }
+
+    public static EntrenadorResponse toDTO(
+        Entrenador entrenador
+    ){
+        return EntrenadorResponse.builder()
+                .id(entrenador.getId())
+                .nombre(entrenador.getNombre())
+                .email(entrenador.getEmail())
+                .tipoPreferido(entrenador.getTipoPreferido())
+                .regionPreferida(entrenador.getRegionPreferida())
+                .pokemonPreferido(
+                        PokemonResponse
+                                .builder()
+                                .id(entrenador.getPokemonPreferido().getIdPokemon())
+                                .nombre(entrenador.getPokemonPreferido().getNombre())
+                                .tipoPrincipal(entrenador.getPokemonPreferido().getTipoPrincipal())
+                                .tipoSecundario(entrenador.getPokemonPreferido().getTipoSecundario())
+                                .spriteNormal(entrenador.getPokemonPreferido().getSpriteNormal())
+                                .spriteShiny(entrenador.getPokemonPreferido().getSpriteShiny())
+                                .build()
+                )
+                .build();
+    }
+
+    public static List<EntrenadorResponse> toDTOList(
+        List<Entrenador> entrenadores
+    ){
+        return entrenadores.stream().map(EntrenadorMapper::toDTO).toList();
+    }
+}
