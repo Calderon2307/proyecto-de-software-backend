edc91e9dcb42a45515b6c5545248ac32e813a948
Calderon0723 <00215818@uca.edu.sv>
2025-11-13 00:41:06 -0600

    ADD: Entrenador DTOs :sparkles:

---
+++ src/main/java/com/software/pokedexV2/dto/response/Entrenador/EntrenadorResponse.java
@@ -0,0 +1,28 @@
+package com.software.pokedexV2.dto.response.Entrenador;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EntrenadorResponse {
+
+    private Long id;
+
+    @JsonProperty(value = "nombre_entrenador")
+    private String nombre;
+
+    @JsonProperty(value = "email")
+    private String email;
+
+    @JsonProperty(value = "pokemon_favorito")
+    private PokemonResponse pokemonPreferido;
+
+    @JsonProperty(value = "region_preferida")
+    private String regionPreferida;
+
+    @JsonProperty(value = "tipo_preferido")
+    private String tipoPreferido;
+}
