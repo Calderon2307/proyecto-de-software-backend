38f128217bcb6677308f9c29197d5e5b33aeb9b9
Calderon0723 <00215818@uca.edu.sv>
2025-11-14 02:55:24 -0600

    ADD: Entrenador Service Interface :sparkles:

---
+++ src/main/java/com/software/pokedexV2/EntrenadorService.java
@@ -0,0 +1,26 @@
+package com.software.pokedexV2;
+
+import com.software.pokedexV2.dto.request.Entrenador.EntrenadorRequest;
+import com.software.pokedexV2.dto.request.Entrenador.EntrenadorUpdateRequest;
+import com.software.pokedexV2.dto.response.Entrenador.EntrenadorResponse;
+
+import java.util.List;
+
+public interface EntrenadorService {
+    //CREATE
+    EntrenadorResponse createEntrenador(EntrenadorRequest entrenadorRequest);
+
+    //READ
+    EntrenadorResponse getByEmail(String email);
+    List<EntrenadorResponse> getAll();
+    List<EntrenadorResponse> getAllByRegionFav(String region);
+    List<EntrenadorResponse> getAllByTipoFav(String tipo);
+    List<EntrenadorResponse> getAllByPokemonFav(String pokemon);
+
+    //UPDATE
+    EntrenadorResponse updateEntrenador(EntrenadorUpdateRequest entrenadorUpdateRequest);
+
+    //DELETE
+    EntrenadorResponse deleteEntrenadorById(String id);
+    EntrenadorResponse deleteEntrenadorByEmail(String email);
+}
