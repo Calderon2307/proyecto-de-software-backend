52cffa8c60ff3b5fb7ccc9cbab4b0544d0a58670
Dmontoya2212 <00087522@uca.edu.sv>
2025-11-14 18:24:48 -0600

    mapper_equipo

--- src/main/java/com/software/pokedexV2/dto/request/Equipo/EquipoUpdateRequest.java
+++ src/main/java/com/software/pokedexV2/dto/request/Equipo/EquipoUpdateRequest.java
@@ -3,6 +3,8 @@ package com.software.pokedexV2.dto.request.Equipo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
