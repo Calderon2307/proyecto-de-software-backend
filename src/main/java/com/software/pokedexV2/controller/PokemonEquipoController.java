ae3d2f9a44a8392769530463452a83fbbcd70718
AllanLopez28 <00049222@uca.edu.sv>
2025-11-17 19:51:21 -0600

    merge

--- src/main/java/com/software/pokedexV2/controller/PokemonEquipoController.java
+++ src/main/java/com/software/pokedexV2/controller/PokemonEquipoController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//pokemon
 @RestController
 @RequestMapping("/pokedexV2/api/pokemon-equipo")
 public class PokemonEquipoController {
